cbcc7d88b77dbabf13aff735582c873efc107264
Andrei Luca <andreilucalink@gmail.com>
2019-12-12 04:21:36 +0200

    firebase apis

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyDzohFsLcKmmAm-ITqSAyve-8D_tNKnR2Y",
+  authDomain: "social-e3588.firebaseapp.com",
+  databaseURL: "https://social-e3588.firebaseio.com",
+  projectId: "social-e3588",
+  storageBucket: "social-e3588.appspot.com",
+  messagingSenderId: "1050651787610",
+  appId: "1:1050651787610:web:e8a6ccada53e6cdefb3363",
+  measurementId: "G-J19HTNT6F1"
+};
